14e1c401bd9fda0458706608422b9d4f838f4eb9
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-10-06 16:41:22 -0300

    feat: adding firebase config example file

---
+++ src/firebase.config.example.ts
@@ -0,0 +1,12 @@
+/* THIS IS AN EXAMPLE FILE */
+/* RENAME THIS FILE TO firebase.config.ts AND FILL IN THE VALUES */
+export const firebaseConfig = {
+    apiKey: "YOUR API KEY",
+    authDomain: "YOUR AUTH DOMAIN",
+    projectId: "YOUR PROJECT ID",
+    storageBucket: "YOUR STORAGE BUCKET",
+    messagingSenderId: "YOUR MESSAGING SENDER ID",
+    appId: "YOUR APP ID",
+    measurementId: "YOUR MEASUREMENT ID"
+};
+
